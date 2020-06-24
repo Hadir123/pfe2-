3e46d6aec3ea968cbc822ce3510e94da496f7e26
saada hadir <saada.hadir9@gmail.com>
2020-06-24 23:21:50 +0200

    first

--- src/app/Services/after-login.service.ts
+++ src/app/Services/after-login.service.ts
@@ -9,12 +9,16 @@ import { AuthService } from './auth.service';
 export class AfterLoginService implements CanActivate {
   authenticationService: any;
 
-  constructor(private Token:TokenService, private route:Router , private authService:AuthService) { }
+  constructor(private Token:TokenService, private route:Router , private authService:AuthService) {
+
+  }
   canActivate(route: import("@angular/router").ActivatedRouteSnapshot, state: import("@angular/router").RouterStateSnapshot): boolean | import("@angular/router").UrlTree | import("rxjs").Observable<boolean | import("@angular/router").UrlTree> | Promise<boolean | import("@angular/router").UrlTree> {
 
+    if (this.Token.loggedIn() && Boolean(localStorage.getItem("logedIn"))){
      return this.Token.loggedIn() ;
+    }else{
+      window.location.replace('/login')
     }
-
-
+  }
 }
 
